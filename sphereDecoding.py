3c30d1cb6472e923771f8d1a1490df488fa66fc6
Sina <sinaee97@gmail.com>
2019-12-29 18:31:59 +0330

    bug for refrences fixed

--- sphereDecoding.py
+++ sphereDecoding.py
@@ -21,13 +21,10 @@ def qDecom(temp) :
 ###Input H(n,m) , d ,x(1,n) , s?(1,m)
 H = np.array([[2, -1, -1] ,[-1, 0 ,  -2],[-1,-1,-1]])
 d = alpha * sigma * n
-s = np.random.random_integers(0,10,m)
-
+s = np.zeros(m)
 v = np.random.normal(0, sigma, n)
 x = np.array([[-1],[1],[0]])
-print(H)
-print(s)
-print(x)
+
 
 babaiB = np.floor(np.dot(np.linalg.pinv(H),x))
 babaiD = np.linalg.norm(x-np.dot(H,babaiB))
@@ -50,27 +47,29 @@ else :
 q1H = q1.conj().T 
 
 y = np.dot(q1H,x)
-_y = y
+_y = y.copy()
 
 minus = 0
 if n != m :
     minus = np.linalg.norm(np.dot(q2H,x))**2
 
 D = np.zeros(m)
+UB = np.zeros(m)
 
 k = m - 1
 D[k] = np.sqrt(d**2 - minus)
-UB = np.zeros(m)
+
 # print(R)
 ###Start
 setUB = 1
+
 answers = []
 flopsCount = 0
 ans = 10000000000
 while True :
     flopsCount += 1
     if setUB == 1:
-        if (D[k] + _y[k]) / R[k][k] >= (-D[k] + _y[k]) / R[k][k] : 
+        if (D[k] + _y[k]) / R[k][k] > (-D[k] + _y[k]) / R[k][k]  : 
             UB[k] = np.floor((D[k] + _y[k]) / R[k][k])
             s[k] = np.ceil((-D[k] + _y[k]) / R[k][k])  - 1
         else :
@@ -78,13 +77,15 @@ while True :
             s[k] = np.ceil((D[k] + _y[k]) / R[k][k])  - 1
         
     s[k] = s[k] + 1
-    print(k,s[k],UB[k])
+    # print(k,s[k],UB[k])
     setUB = 0
     if s[k] <= UB[k] :
         if k == 0 :
-            ans = min(D[m-1]**2 - D[0]**2 + (y[0] - R[0][0]*s[k])**2,ans)
-            print(s,D[m-1]**2 - D[0]**2 + (y[0] - R[0][0]*s[k])**2 )
-            answers.append([s,D[m-1]**2 - D[0]**2 + (y[0] - R[0][0]*s[k])**2])
+            if ans > np.linalg.norm(np.dot(H,s.T)-x.T):
+                ans = np.linalg.norm(np.dot(H,s.T)-x.T)
+                answer = s.copy()
+                # print("***",s,answer)
+            # print(s,np.linalg.norm(np.dot(H,s.T)-x.T) )
         else :
             k = k - 1
             _y[k] = y[k]
@@ -92,9 +93,8 @@ while True :
                 flopsCount += 1
                 _y[k] -= (R[k][i] * s[i])
            
-            # print(D[k+1],(_y[k+1] - R[k+1][k+1] * s[k+1]),D[k+1]**2 - (_y[k+1] - R[k+1][k+1] * s[k+1])**2)    
             D[k] = np.sqrt(D[k+1]**2 - (_y[k+1] - R[k+1][k+1] * s[k+1])**2)
-    
+            # print(D[k+1],D[k])
             setUB = 1
         continue
     else : 
@@ -102,6 +102,10 @@ while True :
         if k == m :
             break
 
-print(flopsCount * 20)
+print("flops: ",flopsCount * 20)
 print(ans)
+print(answer.T)
+# print(np.dot(H,answer.T),x)
+# print(np.dot(H,answer.T) - x.T)
+# print(np.linalg.norm(np.dot(H,answer.T)-x.T))
 # print(answers)
